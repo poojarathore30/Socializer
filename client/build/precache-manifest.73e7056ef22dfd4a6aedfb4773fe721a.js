e35009108d7194eb213bb0c7de32508951f9e2c4
Pooja Rathore <poojarathore.nks@gmail.com>
2020-08-31 11:31:19 +0530

    build success 5

---
+++ client/build/precache-manifest.73e7056ef22dfd4a6aedfb4773fe721a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1de26eba08fb171ae42ff9e5364824bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb9aeb449a9258862d0a",
+    "url": "/static/css/main.28ec31e2.chunk.css"
+  },
+  {
+    "revision": "323dcbdfd29f271938b9",
+    "url": "/static/js/2.185c65b9.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.185c65b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb9aeb449a9258862d0a",
+    "url": "/static/js/main.3cede952.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
